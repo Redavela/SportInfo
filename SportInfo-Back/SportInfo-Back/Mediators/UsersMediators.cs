52072c6a0880789c7cff0c54accc48560c463c3d
Redavela <kodadreda67@gmail.com>
2024-06-06 19:35:42 +0200

    Ajout du mediators + impl controller

---
+++ SportInfo-Back/SportInfo-Back/Mediators/UsersMediators.cs
@@ -0,0 +1,21 @@
+﻿using SportInfo_Back.Mediators.Interfaces;
+using SportInfo_Back.Models;
+using SportInfo_Back.Services.Interfaces;
+
+namespace SportInfo_Back.Mediators
+{
+   
+    public class UsersMediators: IUsersMediators
+    {
+        private readonly IUsersService UsersService;
+
+        public UsersMediators(IUsersService usersService)
+        {
+            UsersService = usersService;
+        }
+        public async Task<User> Create(string username, string password, CancellationToken ctk)
+        {
+            return await UsersService.Create(username, password, ctk);
+        }
+    }
+}
