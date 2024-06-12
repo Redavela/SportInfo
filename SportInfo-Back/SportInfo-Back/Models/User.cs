62f8f8d7da0bf43e74e9c55a900d6e2882bac22a
Redavela <kodadreda67@gmail.com>
2024-06-12 19:53:50 +0200

    Ajout de la propriete Role dans User + création d'un dossier Enums avec un fichier avec les differents role.

--- SportInfo-Back/SportInfo-Back/Models/User.cs
+++ SportInfo-Back/SportInfo-Back/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using SportInfo_Back.Enums;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace SportInfo_Back.Models
@@ -9,6 +10,7 @@ namespace SportInfo_Back.Models
         public int Id { get; set; }
         [Required]
         public string Username { get; set; } = string.Empty;
+        public RoleEnum Role { get; set; } = RoleEnum.User;
         [Required]
         [JsonIgnore]
         public byte[]? PasswordHash { get; set; }
